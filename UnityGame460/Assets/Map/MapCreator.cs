5975b4ee49f400847a5b77a92d2d4aab0e0c8195
Samuel Capotosto <sam-i-am@maine.rr.com>
2015-10-29 11:12:08 -0400

    Map Generator

--- UnityGame460/Assets/Map/MapCreator.cs
+++ UnityGame460/Assets/Map/MapCreator.cs
@@ -4,9 +4,23 @@ using System.Collections;
 public class MapCreator : MonoBehaviour {
     //Test
     //Test again
+    public GameObject groundTile, wallTile;
+    public int width, height;
+
 	// Use this for initialization
 	void Start () {
-	
+	    for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                GameObject created = groundTile;
+                if(Random.Range(0f, 1f) < 0.2f)
+                {
+                    created = wallTile;
+                }
+                Instantiate(created, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
 	}
 	
 	// Update is called once per frame
